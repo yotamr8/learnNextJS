84cd998e4f8fe344f6e130214b03e0acefbeb996
yotam <yotamr8@gmail.com>
2025-02-03 11:45:49 +0200

    Metadata

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,6 +2,28 @@ import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
 import { notFound } from "next/navigation";
+// import { Metadata } from "next";
+
+// export const metadata: Metadata = {
+//   title: "Edit Invoice",
+// };
+
+export async function generateMetadata(props: {
+  params: Promise<{ id: string }>;
+}) {
+  const params = await props.params;
+  const id = params.id;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+
+  return {
+    title: `Edit ${
+      customers.find((c) => c.id == invoice.customer_id)?.name || "Unknown"
+    }'s Invoice`,
+  };
+}
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
